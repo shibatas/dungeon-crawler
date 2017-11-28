77b4df3cccf4ee11554d85537c188f4ab01d834e
Shohei <shohei51@gmail.com>
2017-11-28 20:22:07 +0000

    shows map and player, working on getting keystroke

--- src/App.js
+++ src/App.js
@@ -1,21 +1,127 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Footer from './assets/footer';
 import './App.css';
+import {x, y, mapWidth, emptyMap, initWall} from './config.js';
 
-class App extends Component {
+class Parent extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <App />
+        <Footer />
       </div>
     );
   }
 }
+class Header extends Component {
+    render() {
+      return (
+        <header>
+          <div className="App App-header">
+            <h1 className='App-title'>Dungeon Crawler</h1>
+          </div>
+        </header>
+      );
+    }
+}
 
-export default App;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      map: emptyMap,
+      wall: initWall,
+      player: '27x17'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <input onKeyDown={this.handleKeyPress} />
+        
+        <div onKeyDown={this.handleKeyPress}>
+          {this.generateMap()}
+        </div>
+      </div>
+    );
+  }
+  componentDidUpdate() {
+    //console.log(this.state.map);
+  }
+  generateMap = () => {
+    let map = Object.assign({}, this.state.map);
+    //set class @ player position
+    map[this.state.player] = 'player';
+    
+    //set class for walls
+    this.state.wall.map((id) => {
+      map[id] = 'wall';
+    });
+    
+    //generate map
+    let render = [];
+    let w = mapWidth / x;
+    let h = w;
+    for (let j=0; j<y; j++) {
+      let row = [];
+      for (let i=0; i<x; i++) {
+        let pos = i+'x'+j;
+        row.push(
+          <span key={pos} id={pos} onClick={this.handleClick} >
+            <svg width={w} height={h}>
+              <rect id={pos} className={map[pos]} width={w} height={h} />
+            </svg>
+          </span>
+        );
+      }
+      render.push(
+        <div key={'row'+j} id={'row'+j} style={{height: h}}>{row}</div>
+      );
+    }
+    return render;
+  }
+  handleClick = (e) => {
+    let target = [ e.target.id ];
+    this.update('wall', target);
+  }
+  handleKeyPress = (e) => {
+    console.log('key', e.key);
+  }
+  update = (newClass, target) => {
+    let wall = this.state.wall.slice();
+    let obj = Object.assign({}, this.state.map);
+    target.forEach((item) => {
+      obj[item] = newClass;
+      wall.push(item);
+    });
+    console.log(wall);
+    this.setState({
+      map: obj,
+      wall: wall
+    });
+  }
+}
+
+
+export default Parent;
+/*
+let w = mapWidth / x;
+let h = w;
+let board = [];
+for (let j=0; j<y; j++) {
+  for (let i=0; i<x; i++) {
+    let pos = i+'x'+j;
+    defaultMap.pos = (
+          <span key={pos} id={pos} >
+            <svg width={w} height={h}>
+              <rect id={pos} className={this.state.map[pos]} width={w} height={h} />
+            </svg>
+          </span>
+        );
+    }
+}
+*/
---
+++ src/config.js
@@ -0,0 +1,18 @@
+//number of cells, [x, y]
+const size = [60, 40];
+const x = 60, y = 40;
+//map width
+const mapWidth = 800;
+//initial state of the board (empty board)
+let emptyMap = {};
+for (let j=0; j<size[1]; j++) {
+  for (let i=0; i<size[0]; i++) {
+    emptyMap[i+'x'+j] = "default";
+  }
+}
+
+let initWall = [
+  "23x12", "24x12", "25x12", "26x12", "27x12", "28x12", "29x12", "30x12", "31x12", "31x13", "31x14", "31x15", "31x18", "31x19", "31x20", "31x21", "30x21", "29x21", "28x21", "27x21", "25x21", "26x21", "22x21", "22x19", "22x20", "22x18", "22x17", "22x16", "22x13", "22x12"
+];
+
+export {x, y, mapWidth, emptyMap, initWall};
